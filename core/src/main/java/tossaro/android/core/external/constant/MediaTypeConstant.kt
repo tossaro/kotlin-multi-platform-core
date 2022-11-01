4871aaacc71df4b277fb140b3e66491c42d72016
Hamzah Tossaro <hamzah.tossaro@gmail.com>
2022-11-02 00:51:21 +0700

    - remove unused code

--- core/src/main/java/tossaro/android/core/external/constant/MediaTypeConstant.kt
+++
@@ -1,8 +0,0 @@
-package tossaro.android.core.external.constant
-
-@Suppress("UNUSED")
-object MediaTypeConstant {
-    const val MEDIA_TYPE_TEXT = "text/plain"
-    const val MEDIA_TYPE_IMAGE = "image/*"
-    const val MEDIA_TYPE_FORM_DATA = "multipart/form-data"
-}
--- example/src/main/java/tossaro/android/core/example/app/stockdetailsheet/StockDetailSheetViewModel.kt
+++
@@ -1,9 +0,0 @@
-package tossaro.android.core.example.app.stockdetailsheet
-
-import androidx.lifecycle.MutableLiveData
-import tossaro.android.core.app.common.BaseViewModel
-
-class StockDetailSheetViewModel : BaseViewModel() {
-    val coin = MutableLiveData<String>()
-    val vall = MutableLiveData<String>()
-}
